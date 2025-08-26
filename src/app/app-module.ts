2baff9555ab2a943befc223c1dccfc2a671e7022
Leonidas Caliel <leonidascalieldecarvalho@gmail.com>
2025-08-26 11:12:47 -0300

    forms

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Forms } from './forms/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Forms
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
